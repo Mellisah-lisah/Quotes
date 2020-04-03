335fdc36564d55e96503b94f99c421001ca8eab9
Mellisah-lisah <aliceochieng25@gmail.com>
2020-04-03 23:13:25 +0300

    added changes on the background

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,12 +8,12 @@ import { Quote } from './quote';
 })
 export class AppComponent {
   quotes:Quote[] = [
-    new Quote(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son',new Date(2020,3,14)),
-    new Quote(2,'Buy Cookies','I have to buy cookies for the parrot',new Date(2019,6,9)),
-    new Quote(3,'Get new Phone Case','Diana has her birthday coming up soon',new Date(2022,1,12)),
-    new Quote(4,'Get Dog Food','Pupper likes expensive snacks',new Date(2019,0,18)),
-    new Quote(5,'Solve math homework','Damn Math',new Date(2019,2,14)),
-    new Quote(6,'Plot my world domination plan','Cause I am an evil overlord',new Date(2030,3,14)),
-  ];
+    new Quote(1, 'No human is limited', 'Eliud Kipchoge',new Date(2020,3,14)),
+    new Quote(2,'Great minds discuss ideas; average minds discuss events; small minds discuss people.','Eleanor Roosevelt',new Date(2019,6,9)),
+    new Quote(3,'Do one thing every day that scares you','Eleanor Roosevelt',new Date(2022,1,12)),
+    new Quote(4,'If you always do what interests you, at least one person is pleased','Katharine Hepburn',new Date(2019,0,18)),
+    new Quote(5,'The difference between winning and losing is most often not quitting','Damn Math',new Date(2019,2,14)),
+    new Quote(6,'Things work out best for those who make the best of how things work out','John Wooden',new Date(2030,3,14)),
+  ];  
 }
 
