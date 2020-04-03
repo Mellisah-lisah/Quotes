0b48300b3459eb975295aa1e2e0e4e1182f1bfd3
Mellisah-lisah <aliceochieng25@gmail.com>
2020-04-03 22:07:32 +0300

    added anew quote

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -11,7 +11,7 @@ export class QuoteComponent implements OnInit {
     new Quote(1, 'No human is limited', 'Eliud Kipchoge',new Date(2020,3,14)),
     new Quote(2,'Great minds discuss ideas; average minds discuss events; small minds discuss people.','Eleanor Roosevelt',new Date(2019,6,9)),
     new Quote(3,'Do one thing every day that scares you','Eleanor Roosevelt',new Date(2022,1,12)),
-    new Quote(4,'Get Dog Food','Pupper likes expensive snacks',new Date(2019,0,18)),
+    new Quote(4,'If you always do what interests you, at least one person is pleased','Katharine Hepburn',new Date(2019,0,18)),
     new Quote(5,'Solve math homework','Damn Math',new Date(2019,2,14)),
     new Quote(6,'Plot my world domination plan','Cause I am an evil overlord',new Date(2030,3,14)),
   ];
